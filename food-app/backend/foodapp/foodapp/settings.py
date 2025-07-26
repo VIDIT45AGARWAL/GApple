37f8f51d205e838b6ddef895ae9622b83cb6de5d
Vidit Agarwal <viditagarwal0123@gmail.com>
2025-07-26 11:55:27 +0530

    changes in settings

--- food-app/backend/foodapp/foodapp/settings.py
+++ food-app/backend/foodapp/foodapp/settings.py
@@ -31,7 +31,7 @@ STRIPE_SECRET_KEY=os.getenv('STRIPE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://gapple.onrender.com']
+ALLOWED_HOSTS = ['gapple.onrender.com']
 
 # Application definition
 
